3224c8514c243e7402e4d2edc3eb519ea3308d15
vikramsingh117 <vikramandanshu@gmail.com>
2025-02-07 03:57:14 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Sidebar from "./components/Sidebar";
+import ProductTable from "./components/ProductTable";
+
+function App() {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <ProductTable />
+    </div>
+  );
+}
+
+export default App;
